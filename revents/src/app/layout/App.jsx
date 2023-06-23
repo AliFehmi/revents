fba17da261aceea0991ef66de2c1073ae490d9f0
AliFehmi <alifehmi@sabanciuniv.edu>
2023-06-23 17:22:49 +0300

    End of section 3

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
+import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
+import NavBar from '../../features/events/nav/NavBar';
+import { Container } from 'semantic-ui-react';
 
 function App() {
+  const [formOpen, setFormOpen] = useState(false);
   return (
-    <div className="App">
-      <h1>Re-vents</h1>
+    <div >
+      <NavBar setFormOpen={setFormOpen}></NavBar>
+      <Container className='main'>
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
+      </Container>
     </div>
   );
 }
